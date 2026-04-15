a352cd84750601a86c7f68cbeb2fa3cec050d421
Artyom Nikoghosyan <tyom.nikogosyan@gmail.com>
2026-04-15 21:15:08 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import http from 'http';
+import 'dotenv/config';
+
+const hostname = "localhost"
+const {PORT}=process.env
+const server = http.createServer(async function (req, res) {
+
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('Hello World');
+});
+server.listen(PORT, hostname, () => {
+  console.log(`Server running on${PORT}`);
+});
